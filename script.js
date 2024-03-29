40d079dffcbad5a729bc5bc71263a784d0051ae4
Dnyaneshwar Magar <89983890+dnyaneshwarmagar@users.noreply.github.com>
2024-03-29 14:19:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//your JS code here. If required.
+const currentURL = window.location.href;
+
+// Count the number of letters in the URL using the length property
+const urlLength = currentURL.length;
+
+// Print the final length of the URL using alert()
+alert(`The length of the URL is: ${urlLength}`);
